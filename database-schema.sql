a6f1c38118e69ea684894d6bca409fc873b4ff25
Japheth Mutai <japhemchirie@gmail.com>
2019-11-11 16:54:05 +0300

    Create sql schema for the teamwork app
    The file has 5 tables that will be used to store data by the application.
    Additionally, the has been initialized with npm.

---
+++ database-schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Users (
+	userId SERIAL PRIMARY KEY,
+	firstName VARCHAR (50) NOT NULL,
+    lastName VARCHAR (50) NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    password VARCHAR (255) NOT NULL,
+    gender VARCHAR(10) check (gender in ('Male', 'Female')),
+    jobRole VARCHAR(10) check (jobRole in ('Admin', 'User')),
+    department VARCHAR (100) NOT NULL,
+    address VARCHAR (100) NOT NULL,
+	createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (email)
+	);
+
+CREATE TABLE Gifs (
+	gifId SERIAL PRIMARY KEY,
+	image VARCHAR (50) NOT NULL,
+    title VARCHAR (50) NOT NULL,
+    flags INTEGER DEFAULT 0,
+    userId INTEGER REFERENCES Users(userId),
+	createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
+
+CREATE TABLE Articles (
+	articleId SERIAL PRIMARY KEY,
+	title VARCHAR (100) NOT NULL,
+    article TEXT NOT NULL,
+    flags INTEGER DEFAULT 0,
+    userId INTEGER REFERENCES Users(userId),
+	createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
+
+CREATE TABLE Category (
+	categoryId SERIAL PRIMARY KEY,
+	category VARCHAR (50) NOT NULL,
+    articleId INTEGER REFERENCES Articles(articleId),
+	createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
+
+CREATE TABLE Comments (
+	commentId SERIAL PRIMARY KEY,
+	comment TEXT NOT NULL,
+    articleId INTEGER REFERENCES Articles(articleId),
+	createdOn TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	);
